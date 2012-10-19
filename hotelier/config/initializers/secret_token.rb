caa125f01a90c18c4d04a54e69ba45986fb6af41
LVZ <laura.veazey@gmail.com>
2012-10-18 19:16:15 -0500

    Initial commit

---
+++ hotelier/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotelier::Application.config.secret_token = '8905864103831ebb5c9ec94f399e3ac02099dc66eeaa8e9d9974ca7275c277669a74664d58d19939206b28007cf79bbf28d712b3672394cd34a3de9535fd7022'
